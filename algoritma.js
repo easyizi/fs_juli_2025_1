f6462a445304d09154f43d205b5b484bbe919506
FAIZI KHOLILULLAH <815101321@intranet.pajak.go.id>
2025-07-05 14:16:55 +0700

    membuat algoritma dan pseucode

---
+++ algoritma.js
@@ -0,0 +1,31 @@
+// tentukan persegi panjang dengan algortima dan pseucode
+// untuk menghitung persegi panjang
+// algoritma
+// 1. buat panjang
+// 2. buat lebar
+// 3. buat luas dengan mengkalikan panjang kali lebar
+// 4. cetak luas
+
+
+// pseucude
+// isi panjang dengan angka
+// isi lebar dengan angka 
+// isi luas dengan panjang * lebar
+// cetak luas 
+
+
+// tentukan algortima dan pseucode untuk luas lingkaran
+
+
+//algoritma 
+// 1. buat lingkarang
+// 2. tentukan jari2
+// 3. buat phi 22/7
+
+//pseucode 
+//1. isi jari/r dengan angka
+//2. isi phi dengan angka 22/7 atau 3,14
+//3. isi luas lingkarang dengan phi kali r kali r
+//4. cetak luas lingkaran
+
+
