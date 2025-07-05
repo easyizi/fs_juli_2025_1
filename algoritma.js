9fd12c498276ac9ae8f75f79b02b041c504b25c0
FAIZI KHOLILULLAH <815101321@intranet.pajak.go.id>
2025-07-05 15:11:17 +0700

    membuat algoritma dan pseucode upadte

--- algoritma.js
+++ algoritma.js
@@ -47,5 +47,50 @@
 // 2. isi variabel 2/3
 // 3. isi variabel jari
 // 4. isi variabel phi
-// 5. isi variabel luas dengan 2/3 * 1/2 * phi * r3
+// 5. isi variabel luas dengan 2/3 * 1/2 * phi * r* r *r
 // 6. cetak luas
+
+/**
+ * STUDY CASE 4
+ *
+ *  Tentukan algoritma dan pseudocode luas permukaan tabung
+ *
+ * ALGORITMA
+ * 1. Buat variable phi isi dengan 3.14
+ * 2. Buat variable r
+ * 3. Buat variable t
+ * 4. Buat variable luas
+ * 5. Isi luas dengan 2 kali phi kali r kali t kali ( r tambah t )
+ * 6. Cetak luas
+ *
+ * PSEUDOCODE
+ * STORE phi WITH 3.14
+ * STORE r WITH ANY NUMBER
+ * STORE t WITH ANY NUMBER
+ *
+ * STORE luas WITH 2 MUL phi MUL r MUL t MUL ( r ADD t )
+ * PRINT luas
+ */
+
+
+/**
+ * STUDY CASE 5
+ * 
+ *  Tentukan algortima dan pseucode luas permukaan balok
+ * 
+ *  Algortima
+ *  1. buat panjang dengan p
+ *  2. buat lebar dengan l
+ *  3. buat tinggi dengan t
+ *  4. buat variabel luas permukaan
+ *  5. isi luas permukaan dengan 2 kali (p kali l) tambah (p kali t) tambah (l kali t)
+ *  6. cetak luas
+ * 
+ * PSEUCODE
+ * STORE phi with 3.14
+ * STORE p with ANY NUMBER
+ * STORE l with ANY NUMBER
+ * STORE t with ANY NUMBER
+ * 
+ * STORE luas WITH 2 MUL p
+ */
