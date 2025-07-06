c2ceeb36e20dabe78398de9d13ebd43359a4be76
FAIZI KHOLILULLAH <815101321@intranet.pajak.go.id>
2025-07-06 13:37:01 +0700

    conditional

--- conditional.js
+++ conditional.js
@@ -22,3 +22,89 @@ if (angka = 18) {
 } else {
     console.log ("Belum bisa bikin KTP");
 }
+
+/**
+ * 
+ * SOAL 
+ * Bikinlah kode untuk memilih minuman. 
+ * Minuman yang tersedia adalah kopi dan 
+ * teh dengan switch case
+ * 
+ */
+
+let minuman = "kopi";
+switch (minuman) {
+    case 'kopi':
+        console.log ('saya pilih kopi');
+        break;
+    case 'teh':
+        console.log ('saya pilih teh');
+        break;
+}
+
+
+
+/** KONDISI BERSARANG
+ * 
+ * Buatlah sebuah aplikasi tiket masuk sebuah wahana 
+ * dengan ketentuan sebagai berikut:
+ * Jika anak berumur di bawah 17 tahun,
+ * Jika tinggi anak di bawah 150cm, maka cetak 
+ * “Tidak boleh masuk”
+ * Jika tinggi anak di atas sama dengan 150cm maka cetak “Boleh masuk”
+ * 
+ */
+
+let umur = 17;
+let tinggi = 155;
+
+if (umur < 17 ) {
+    if ( tinggi < 150 ) {
+        console.log ("Tidak Boleh Masuk")
+    }
+    
+} else {
+    console.log ("Boleh Masuk")
+}
+
+
+/**
+ * /**
+ * * Study Case 1
+ *
+ * Cek apakah sebuah bilangan genap atau ganjil
+ * Tentukan algoritma, pseudocode dan coding
+ * 
+ * 
+ * ALGORITMA
+ * 1. Ambil input bilangan dari pengguna
+ * 2. cek apakah bilangan habis dibagi 2 (bilangan % 2 == 0)
+ * 3. jika habis maka genap
+ * 4. jika habis maka ganjil
+ * 5. tentukan angkanya
+ * 6. cetak hasilnya
+ * 
+ * PSEUCODE
+ * 
+ * BEGIN
+ *   STORE number WITH ANY NUMBER
+ * 
+ * If number MOD 2 EQUAL 0 THEN
+ *   PRINT "GENAP"
+ * ELSE IF number MOD 2 EQUAL 1 THEN
+ *  PRINT "GANJIL"
+ * 
+ * ENDIF
+ * END
+ * 
+ */
+
+let bilangan = 11;
+if (bilangan % 2 == 0) {
+    console.log ("bilangan genap")
+} else {
+    console.log ("bilangan ganjil")
+}
+
+
+
